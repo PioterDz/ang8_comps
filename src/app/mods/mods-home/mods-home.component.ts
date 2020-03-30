84d4248c996513718d2d8ea9d6f538c5882e97fc
Piotr Dzienis <dojlidy.zubr@gmail.com>
2020-03-30 20:32:27 +0200

    finish modules

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModsHomeComponent implements OnInit {
   modalOpen = false;
+  items = [
+    { title: 'Why is the sky blue?', content: 'Because it is.' },
+    { title: 'What is orange?', content: 'It is fruit.' },
+    { title: 'What color is that cat?', content: 'It\'s black and yellow' }
+  ]
 
   constructor() { }
 
